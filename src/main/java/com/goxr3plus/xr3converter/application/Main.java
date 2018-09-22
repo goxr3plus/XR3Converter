8f35cdf1bad9bd1f542fce882cec3fdf38d72695
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-09-22 21:26:09 +0300

    First code

---
+++ src/main/java/com/goxr3plus/xr3converter/application/Main.java
@@ -0,0 +1,17 @@
+package main.java.com.goxr3plus.xr3converter.application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
