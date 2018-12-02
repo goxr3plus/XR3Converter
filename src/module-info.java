d3c8f553a15e081f5a4ede0e9a6810aca61c34a2
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-12-02 23:01:44 +0200

    Passing to Java 11

---
+++ src/module-info.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+/**
+ * @author GOXR3PLUSSTUDIO
+ *
+ */
+module xR3Converter {
+	exports main.java.com.goxr3plus.xr3converter.storage;
+	exports main.java.com.goxr3plus.xr3converter.tools.general;
+	exports main.java.com.goxr3plus.xr3converter.converter.controller;
+	exports main.java.com.goxr3plus.xr3converter.tools.fx;
+	exports main.java.com.goxr3plus.xr3converter.converter.service;
+	exports main.java.com.goxr3plus.xr3converter.tools.env;
+	exports main.java.com.goxr3plus.xr3converter.tools.io;
+	exports main.java.com.goxr3plus.xr3converter.converter.model;
+	exports main.java.com.goxr3plus.xr3converter.application;
+
+	requires java.desktop;
+	requires java.logging;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+}
